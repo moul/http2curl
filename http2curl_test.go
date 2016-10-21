586f020981d42b1057d92267e9add49645b1e088
Drew LeSueur <drew.lesueur@gpsinsight.com>
2016-10-21 12:21:54 -0700

    Remove testing dependencies

--- http2curl_test.go
+++ http2curl_test.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func ExampleGetCurlCommand() {
@@ -21,18 +19,22 @@ func ExampleGetCurlCommand() {
 }
 
 func TestGetCurlCommand(t *testing.T) {
-	Convey("Testing http2curl", t, func() {
-		uri := "http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu"
-		payload := new(bytes.Buffer)
-		payload.Write([]byte(`{"hello":"world","answer":42}`))
-		req, err := http.NewRequest("PUT", uri, payload)
-		So(err, ShouldBeNil)
-		req.Header.Set("X-Auth-Token", "private-token")
-		req.Header.Set("Content-Type", "application/json")
+	uri := "http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu"
+	payload := new(bytes.Buffer)
+	payload.Write([]byte(`{"hello":"world","answer":42}`))
+	req, err := http.NewRequest("PUT", uri, payload)
+	if err != nil {
+		t.Fatalf("got error with request: %v", err)
+	}
+	req.Header.Set("X-Auth-Token", "private-token")
+	req.Header.Set("Content-Type", "application/json")
 
-		command, err := GetCurlCommand(req)
-		So(err, ShouldBeNil)
-		expected := `curl -X PUT -d "{\"hello\":\"world\",\"answer\":42}" -H "Content-Type: application/json" -H "X-Auth-Token: private-token" 'http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu'`
-		So(command.String(), ShouldEqual, expected)
-	})
+	command, err := GetCurlCommand(req)
+	if err != nil {
+		t.Fatalf("got error with GetCurlCommand: %v", err)
+	}
+	expected := `curl -X PUT -d "{\"hello\":\"world\",\"answer\":42}" -H "Content-Type: application/json" -H "X-Auth-Token: private-token" 'http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu'`
+	if command.String() != expected {
+		t.Fatalf("\nexpected: %s\ngot     : %s", expected, command.String())
+	}
 }
