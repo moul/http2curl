e02c7eb1754195f07a8bce79bda458e7e73e08d4
Manfred Touron <m@42.am>
2016-10-24 12:03:44 +0200

    Revert "Remove testing dependencies"
    This reverts commit 586f020981d42b1057d92267e9add49645b1e088.

--- http2curl_test.go
+++ http2curl_test.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"net/url"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func ExampleGetCurlCommand() {
@@ -82,22 +84,18 @@ func ExampleGetCurlCommand_specialCharsInBody() {
 }
 
 func TestGetCurlCommand(t *testing.T) {
-	uri := "http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu"
-	payload := new(bytes.Buffer)
-	payload.Write([]byte(`{"hello":"world","answer":42}`))
-	req, err := http.NewRequest("PUT", uri, payload)
-	if err != nil {
-		t.Fatalf("got error with request: %v", err)
-	}
-	req.Header.Set("X-Auth-Token", "private-token")
-	req.Header.Set("Content-Type", "application/json")
+	Convey("Testing http2curl", t, func() {
+		uri := "http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu"
+		payload := new(bytes.Buffer)
+		payload.Write([]byte(`{"hello":"world","answer":42}`))
+		req, err := http.NewRequest("PUT", uri, payload)
+		So(err, ShouldBeNil)
+		req.Header.Set("X-Auth-Token", "private-token")
+		req.Header.Set("Content-Type", "application/json")
 
-	command, err := GetCurlCommand(req)
-	if err != nil {
-		t.Fatalf("got error with GetCurlCommand: %v", err)
-	}
-	expected := `curl -X 'PUT' -d '{"hello":"world","answer":42}' -H 'Content-Type: application/json' -H 'X-Auth-Token: private-token' 'http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu'`
-	if command.String() != expected {
-		t.Fatalf("\nexpected: %s\ngot     : %s", expected, command.String())
-	}
+		command, err := GetCurlCommand(req)
+		So(err, ShouldBeNil)
+		expected := `curl -X 'PUT' -d '{"hello":"world","answer":42}' -H 'Content-Type: application/json' -H 'X-Auth-Token: private-token' 'http://www.example.com/abc/def.ghi?jlk=mno&pqr=stu'`
+		So(command.String(), ShouldEqual, expected)
+	})
 }
